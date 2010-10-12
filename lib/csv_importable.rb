525e4933c5c48646904ea35c8dad578ec01824ac
Tomas Kramar <kramar.tomas@gmail.com>
2010-10-12 17:43:24 +0200

    New style cleaning

--- lib/csv_importable.rb
+++ lib/csv_importable.rb
@@ -4,7 +4,7 @@ module CSVImportable
   end
 
   def csv_columns columns
-    @columns = columns
+    @csv_columns = columns
   end
 
   def load_csv_data
@@ -12,7 +12,40 @@ module CSVImportable
 
     fast_import(csv, :fields_terminated_by => ',' ,
                      :fields_optionally_enclosed_by => '"',
-                     :columns => @columns,
+                     :columns => @csv_columns,
                      :ignore_lines => 1)
+
+    invoke_after_import_callbacks
+  end
+
+  def after_import method
+    @after_import_callbacks ||= []
+    @after_import_callbacks << method
+  end
+
+  def self.extended(base)
+    base.extend Cleaning::CurrencyNormalizer
+  end
+
+  private
+
+  def invoke_after_import_callbacks
+    @after_import_callbacks.each { |callback| send callback }
+  end
+
+  module Cleaning
+    module CurrencyNormalizer
+
+      def currency_columns currency_columns
+        @currency_columns = currency_columns
+      end
+
+      def normalize_currency
+        @currency_columns ||= [:amount]
+        currency_column_update = @currency_columns.map { |c| "#{c.to_s} = #{c.to_s}/30.126" }.join(',')
+
+        update_all("currency = 'EUR', #{currency_column_update}", :currency => ['Sk', 'SKK', 'Skk'])
+      end
+    end
   end
 end
