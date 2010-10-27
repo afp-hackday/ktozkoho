a2a1c38f256c9d6bf497a5b88d8a21df15f438c8
Tomas Kramar <kramar.tomas@gmail.com>
2010-10-28 00:11:15 +0200

    Import procurements from multiple CSVs

--- app/models/datanest/procurement.rb
+++ app/models/datanest/procurement.rb
@@ -1,6 +1,21 @@
 class Datanest::Procurement < ActiveRecord::Base
   extend Datanest::Import
 
-  csv            'procurements_2-dump.csv'
+  csv 'procurements_2-dump.csv'
+  csv 'procurements-dump.csv',
+        :year => 11,
+        :bulletin_id => 12,
+        :procurement_id => 13,
+        :customer_ico => 20,
+        :customer_company_name => 19,
+        :supplier_ico => 22,
+        :supplier_company_name => 21,
+        :supplier_region => 23,
+        :procurement_subject => 14,
+        :price_amount => 15,
+        :currency => 16,
+        :is_VAT_included => 17,
+        :source_url => 18
+
   before_create  :convert_financial_attributes, :empty_attributes_to_null
 end
