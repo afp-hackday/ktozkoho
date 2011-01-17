9789803c024384c5e75a0347b94589279275cb3e
Michal Barla <barla@fiit.stuba.sk>
2011-01-18 00:01:18 +0100

    Put nice title to the portfolio pie chart

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,11 +1,10 @@
 class PartiesController < ApplicationController
 
   def show
-
+    @party = Party.find(params[:id])
   end
 
   def portfolio
-    
     respond_to do |format|
       format.json {render :json => {'Tomas' => 50, 'Dusan' => 20}.to_json}
     end
